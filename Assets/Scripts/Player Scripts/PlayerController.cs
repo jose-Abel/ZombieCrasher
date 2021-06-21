204d88ee1662541c7bbd5940e63d07c70b7454c4
Jose Abel <joseabel.ramirezfrontany@gmail.com>
2021-06-21 00:16:16 -0400

    Create the playercontroller script

--- Assets/Scripts/Player Scripts/PlayerController.cs
+++ Assets/Scripts/Player Scripts/PlayerController.cs
@@ -2,17 +2,67 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour
+public class PlayerController : BaseController
 {
-    // Start is called before the first frame update
+    private Rigidbody myBody;
+
     void Start()
     {
-        
+        myBody = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        ControlMovementWithKeyboard();
+    }
+
+    void FixedUpdate() {
+        MoveTank();
+    }
+
+    void MoveTank() {
+        myBody.MovePosition(myBody.position + speed * Time.deltaTime);
+    }
+
+    void ControlMovementWithKeyboard() {
+        if(Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A)) {
+
+            MoveLeft();
+        }
+
+        if(Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D)) {
+            
+            MoveRight();
+        }
+
+        if(Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.W)) {
+            
+            MoveFast();
+        }
+
+        if(Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.S)) {
+            
+            MoveSlow();
+        }
+
+        if(Input.GetKeyUp(KeyCode.LeftArrow) || Input.GetKeyUp(KeyCode.A)) {
+            
+            MoveStraight();
+        }
+
+        if(Input.GetKeyUp(KeyCode.RightArrow) || Input.GetKey(KeyCode.D)) {
+            
+            MoveStraight();
+        }
+
+        if(Input.GetKeyUp(KeyCode.UpArrow) || Input.GetKey(KeyCode.W)) {
+            
+            MoveNormal();
+        }
+
+       if(Input.GetKeyUp(KeyCode.DownArrow) || Input.GetKey(KeyCode.S)) {
+            
+            MoveNormal();
+        }
     }
 }
