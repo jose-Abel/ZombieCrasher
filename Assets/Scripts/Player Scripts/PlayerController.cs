31c61a123b69ed39027e84cb90304dd35fcc2356
Jose Abel <joseabel.ramirezfrontany@gmail.com>
2021-09-18 08:58:42 -0400

    Create bullet script, helper script, obstacles scripts. Place zombies prefabs in the scene

--- Assets/Scripts/Player Scripts/PlayerController.cs
+++ Assets/Scripts/Player Scripts/PlayerController.cs
@@ -6,6 +6,12 @@ public class PlayerController : BaseController
 {
     private Rigidbody myBody;
 
+    public Transform bullet_StartPoint;
+
+    public GameObject bullet_Prefab;
+
+    public ParticleSystem shootFX;
+
     void Start()
     {
         myBody = GetComponent<Rigidbody>();
@@ -15,6 +21,8 @@ public class PlayerController : BaseController
     {
         ControlMovementWithKeyboard();
         ChangeRotation();
+
+        ShootingControl();
     }
 
     void FixedUpdate() {
@@ -76,6 +84,16 @@ public class PlayerController : BaseController
         }
         else {
             transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.Euler(0f, 0f, 0f), Time.deltaTime * rotationSpeed);            
+        }
+    }
+
+    public void ShootingControl() {
+        if(Input.GetMouseButtonDown(0)) {
+            GameObject bullet = Instantiate(bullet_Prefab, bullet_StartPoint.position, Quaternion.identity);
+
+            bullet.GetComponent<BulletScript>().Move(2000f);
+
+            shootFX.Play();
         }
     }
 }
